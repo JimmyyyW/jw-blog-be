df28dd1fe2a67db70232c6fe02ac3b502517eb31
James Whitfield <user@james-mbp.home>
2020-09-05 16:50:00 +0100

    normalise port

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // app.get('/', (req, res) => {
 //     res.send('Bello');
@@ -11,5 +12,5 @@ app.get('/', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'pages/index.html'))
 });
 
-app.listen(5000, () => console.log('Server running'));
+app.listen(PORT, () => console.log('Server running'));
 
