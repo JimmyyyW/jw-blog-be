3b7439e090dd91b30342a24af494c9182eec74af
James Whitfield <user@james-mbp.home>
2020-09-05 18:13:29 +0100

    added ts tests

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import path from 'path';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
+
+const dbString = process.env['DB_CONNECTION_STRING'];
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -12,5 +16,6 @@ app.get('/', (request, response) => {
     response.sendFile(path.resolve('src', 'pages/index.html'))
 });
 
+console.log(dbString);
 app.listen(PORT, () => console.log('Server running'));
 
---
+++ test/mocha.test.ts
@@ -0,0 +1,10 @@
+import assert from 'assert';
+
+describe('Simple math test for mocha', () => {
+    it('should return 2', () => {
+        assert.equal(1 + 1, 2);
+    });
+    it('should return 9', () => {
+        assert.equal(3 * 3, 9);
+    });
+});
