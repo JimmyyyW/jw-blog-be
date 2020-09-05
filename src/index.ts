d3db90d8860f78052892888c6f77f8f006eb13d0
James Whitfield <user@james-mbp.home>
2020-09-05 17:36:28 +0100

    change / to home

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 5000;
 // });
 
 app.get('/home', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    response.send('Hello');
+    //response.sendFile(path.resolve(__dirname, 'pages/index.html'))
 });
 
 app.listen(PORT, () => console.log('Server running'));
